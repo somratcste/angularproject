097a988af777ffc3c26ea24e0b18e02594a62ffb
somratcste <somratcste@gmail.com>
2017-04-16 11:16:33 +0530

    use chaining method

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
  * Created by somrat on 4/16/17.
  */
 
-var myApp = angular.module('myModule', []) ;
-
-myApp.controller('myController',function ($scope) {
-    $scope.message = 'Angular JS Tutorial' ;
-});
+var myApp = angular
+                .module('myModule', [])
+                .controller('myController' , function ($scope) {
+                    $scope.message = 'Angular JS Tutorial' ;
+                });
