6a07643c255e558a9b7dfbb0d90d5d3cfaff3e28
somratcste <somratcste@gmail.com>
2017-04-16 11:01:55 +0530

    Second tuts

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by somrat on 4/16/17.
+ */
+
+var myApp = angular.module('myModule', []) ;
+
+myApp.controller('myController',function ($scope) {
+    $scope.message = 'Angular JS Tutorial' ;
+});
