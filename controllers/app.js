579508ee4f8cfa4865fa74491366eca71ab61523
somratcste <somratcste@gmail.com>
2017-08-03 22:57:56 +0530

    add controller model

--- controllers/app.js
+++ controllers/app.js
@@ -1,3 +1,11 @@
-/**
- * Created by somrat on 8/3/17.
- */
+var myApp = angular.module("myApp", ["ngRoute"]);
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "templates/list.html",
+            controller : "studentController"
+        })
+        .when("/student/create", {
+            templateUrl : "blue.htm"
+        });
+});
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,8 @@
 /**
  * Created by somrat on 8/3/17.
  */
+myApp.controller('studentController', function($scope) {
+    $scope.getStudents = function () {
+        $scope.students = [ {name:'sormat','name':'akash'}];
+    }
+});
